337604da24beade86ed2837d5298c71b11534d60
Fatma-M <fatmaamohamed941@gmail.com>
2023-10-11 13:38:38 +0300

    add job and job details pages

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_page():
-  return render_template("index.html")
+    return render_template("index.html")
