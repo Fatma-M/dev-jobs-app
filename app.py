57b9dcf3eb1ae891a2771b5d7590c679897c78bb
Fatma-M <fatmaamohamed941@gmail.com>
2023-10-11 16:14:47 +0300

    add pages routes

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+
+@app.route("/add-job")
+def add_job_page():
+    return render_template("add-job.html")
+
+
+@app.route("/job-details")
+def job_details_page():
+    job_title = request.args.get("job-title")
+    return render_template("job-details.html", job_title=job_title)
