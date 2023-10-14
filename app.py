5e7825e722e0b842109c6c6ed52f08415ae7d800
Fatma-M <fatmaamohamed941@gmail.com>
2023-10-14 11:22:17 +0300

    update home page with data from csv file

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
+# CREATE ROUTE FOR DATA TO USE IN JS CODE
+@app.route('/get_data', methods=['GET'])
+def get_csv_data():
+    data = []
+    with open('./static/data.csv', mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+            
+    return data
 
+
+# CREATE HOME ROUTE WITH index.html
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
-
+# CREATE ADD JOB JOB ROUTE
 @app.route("/add-job")
 def add_job_page():
     return render_template("add-job.html")
 
-
+# CREATE JOB DETAILS ROUTE
 @app.route("/job-details")
 def job_details_page():
-    job_title = request.args.get("job-title")
-    return render_template("job-details.html", job_title=job_title)
+    return render_template("job-details.html")
 
-
+# RUN APP WITH DEBUG MODE
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+// UI ELEMENTS
+const jobsContainer = document.getElementById("jobs-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+
+// GLOBAL VARIABLES
+let data;
+
+// CREATE DIV ELEMENTS FOR JOB CARD
+function createDiv(element) {
+  const div = document.createElement("div");
+  div.className =
+    "card bg-white p-6 relative flex flex-col items-start justify-between rounded-md mt-[50px]";
+  div.innerHTML = `
+                  <!-- card image -->
+                  <div
+                    class="card-image w-[50px] h-[50px] flex items-center rounded-2xl absolute top-[-25px]" style="background: ${element.logoBackground}" >
+                    <img src="${element.logo}" alt="" class="block mx-auto"/>
+                  </div>
+                  <!-- card details -->
+                  <div class="time mt-6">
+                    <span class="text-darkGray">${element.postedAt}</span>
+                    <span class="text-darkGray">.</span>
+                    <span class="text-darkGray">${element.contract}</span>
+                  </div>
+                  <div class="title font-bold my-1">
+                    <a class="text-lg" href="/job-details/${element.id}">${element.position}</a>
+                  </div>
+                  <div class="companyName">
+                    <span class="text-darkGray">${element.company}</span>
+                  </div>
+                  <div class="companyLocation mt-8">
+                    <span class="text-darkBlue font-bold">${element.location}</span>
+                  </div>
+        `;
+  jobsContainer.appendChild(div);
+}
+
+// UPDATE HOME PAGE WITH DATA FROM THE DATA ROUTE
+function updateUI() {
+  fetch("/get_data")
+    .then((response) => response.json())
+    .then((items) => {
+      data = items;
+      const temp = data.slice(0, 9);
+      temp.forEach((element) => {
+        createDiv(element);
+      });
+    })
+
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+// HANDLE LOAD MORE DATA
+function loadMore() {
+  const temp = data.slice(9);
+
+  temp.forEach((element) => {
+    createDiv(element);
+  });
+
+  loadMoreBtn.style.display = "none";
+}
+
+// FUNCTION CALLS AND EVENT LISTENERS
+updateUI();
+loadMoreBtn.addEventListener("click", loadMore);
